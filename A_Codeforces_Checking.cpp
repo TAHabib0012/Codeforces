cb692587f1ea57ea5accc31e39dd5aadd18a1353
Tahmid Ahmed Habib <tahmidahmed0012@gmail.com>
2024-07-06 21:08:26 +0600

    Solved

---
+++ A_Codeforces_Checking.cpp
@@ -0,0 +1,52 @@
+/*............Bismillahir Rahmanir Raheem...............*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define Tahmid ios::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr);
+#define LL long long int
+#define ULL unsigned long long 
+#define endl '\n'
+
+#define mem(v, x) memset(v, x, sizeof(v))
+#define ceiling(a, b) ((a / b) + (a % b != 0))
+#define gcd(a, b) __gcd(a, b)
+#define lcm(a, b) ((a * b) / gcd(a, b))
+
+#define yes cout << "YES" << endl;
+#define no cout << "NO" << endl;
+#define HA " "
+
+const LL NN = 1e9 + 6 + 9;
+const LL mod = 998244353;
+
+void ans()
+{
+    char n;
+    cin >> n;
+    string s = "codeforces";
+    auto it = find(s.begin(), s.end(), n);
+    if (it != s.end()) 
+    {
+        yes;
+    }
+    else
+    {
+        no;
+    }
+}
+
+int main() 
+{
+    Tahmid;
+
+    int t;
+    cin >> t;
+ 
+    while(t--)
+    {
+        ans();
+    }
+ 
+    return 0;
+}
