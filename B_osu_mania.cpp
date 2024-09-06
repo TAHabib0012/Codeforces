9c3f788c2b6240ef632a2e2aa608d023efbdc0a5
Tahmid Ahmed Habib <tahmidahmed0012@gmail.com>
2024-09-07 01:51:12 +0600

    Solved

---
+++ B_osu_mania.cpp
@@ -0,0 +1,62 @@
+/*............Bismillahir Rahmanir Raheem...............*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define Tahmid ios::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr);
+#define LL long long int
+#define ULL unsigned long long 
+#define endl '\n'
+
+#define mem(v, x) memset(v, x, sizeof(v))
+#define ceiling(a, b) ((a / b) + (a % b != 0))
+#define gcd(a, b) __gcd(a, b)
+#define lcm(a, b) ((a * b) / gcd(a, b))
+
+#define yes cout << "YES" << endl;
+#define no cout << "NO" << endl;
+#define HA " "
+
+const LL NN = 1e9 + 6 + 9;
+const LL mod = 998244353;
+
+void ans()
+{
+    int n;
+    cin >> n;
+    vector<int> a;
+    for (int i = 0; i < n; i++)
+    {
+        string s;
+        cin >> s;
+        for (int j = 0; j < s.size(); j++)
+        {
+            if (s[j] == '#')
+            {
+                a.push_back(j + 1);
+                break;
+            }
+            
+        }
+    }
+    for (int i = n - 1; i >= 0; i--)
+    {
+        cout << a[i] << HA;
+    }
+    cout << endl;
+}
+
+int main() 
+{
+    Tahmid;
+
+    int t;
+    cin >> t;
+ 
+    while(t--)
+    {
+        ans();
+    }
+ 
+    return 0;
+}
