59e67521ca741b136bca6359cd951c9b1d7bae61
Tahmid Ahmed Habib <tahmidahmed0012@gmail.com>
2024-09-21 22:16:10 +0600

    Solved

---
+++ A_Robin_Helps.cpp
@@ -0,0 +1,63 @@
+/*............Bismillahir Rahmanir Raheem...............*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define Tahmid ios::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr);
+#define LL long long int
+#define ULL unsigned long long 
+#define endl '\n'
+
+#define mem(v, x) memset(v, x, sizeof(v))
+#define ceiling(a, b) ((a / b) + (a % b != 0))
+#define gcd(a, b) __gcd(a, b)
+#define lcm(a, b) ((a * b) / gcd(a, b))
+
+#define yes cout << "YES" << endl;
+#define no cout << "NO" << endl;
+#define HA " "
+
+const LL NN = 1e9 + 6 + 9;
+const LL mod = 998244353;
+
+void ans()
+{
+    int n, m, c = 0, s = 0;
+    cin >> n >> m;
+    vector<int> a(n);
+
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a[i];
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        if(a[i] >= m)
+        {
+            s =  s + a[i];
+        }
+        if (a[i] == 0 && s > 0)
+        {
+            s--;
+            c++;
+        }
+    }
+    
+    cout << c << endl;
+}
+
+int main() 
+{
+    Tahmid;
+
+    int t;
+    cin >> t;
+ 
+    while(t--)
+    {
+        ans();
+    }
+ 
+    return 0;
+}
