94c112adaf66e9d3e61230be0159fb3e9f52626b
Tahmid Ahmed Habib <tahmidahmed0012@gmail.com>
2024-09-21 20:46:38 +0600

    Solved

---
+++ B_Battle_for_Survive.cpp
@@ -0,0 +1,55 @@
+/*............Bismillahir Rahmanir Raheem...............*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define Tahmid ios::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr);
+#define LL long long int
+#define ULL unsigned long long 
+#define endl '\n'
+
+#define mem(v, x) memset(v, x, sizeof(v))
+#define ceiling(a, b) ((a / b) + (a % b != 0))
+#define gcd(a, b) __gcd(a, b)
+#define lcm(a, b) ((a * b) / gcd(a, b))
+
+#define yes cout << "YES" << endl;
+#define no cout << "NO" << endl;
+#define HA " "
+
+const LL NN = 1e9 + 6 + 9;
+const LL mod = 998244353;
+
+void ans()
+{
+    LL n, c = 0;
+    cin >> n;
+    vector<LL> a(n);
+
+    for (LL i = 0; i < n; i++)
+    {
+        cin >> a[i];
+    }
+    
+    c = a[n - 2];
+    for (LL i = 0; i < n - 2; i++)
+    {
+        c = c - a[i];
+    }
+    cout << a[n - 1] - c << endl;
+}
+
+int main() 
+{
+    Tahmid;
+
+    int t;
+    cin >> t;
+ 
+    while(t--)
+    {
+        ans();
+    }
+ 
+    return 0;
+}
