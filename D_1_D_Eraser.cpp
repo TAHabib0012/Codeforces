763548a17f199452d07bad730b27fc8d074ffd00
Tahmid Ahmed Habib <tahmidahmed0012@gmail.com>
2024-09-24 21:08:11 +0600

    Solved

---
+++ D_1_D_Eraser.cpp
@@ -0,0 +1,58 @@
+/*............Bismillahir Rahmanir Raheem...............*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define Tahmid ios::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr);
+#define LL long long int
+#define ULL unsigned long long 
+#define endl '\n'
+
+#define mem(v, x) memset(v, x, sizeof(v))
+#define ceiling(a, b) ((a / b) + (a % b != 0))
+#define gcd(a, b) __gcd(a, b)
+#define lcm(a, b) ((a * b) / gcd(a, b))
+
+#define yes cout << "YES" << endl;
+#define no cout << "NO" << endl;
+#define HA " "
+
+const LL NN = 1e9 + 6 + 9;
+const LL mod = 998244353;
+
+void ans()
+{
+    int n , k, c=0;
+    cin >> n >> k;
+
+    string s;
+    cin >> s;
+    for (int i = 0; i < n;)
+    {
+        if (s[i] == 'B')
+        {
+            c++;
+            i = i + k;
+        }
+        else
+        {
+          i++;
+        }
+    }
+    cout << c << endl;
+}
+
+int main() 
+{
+    Tahmid;
+
+    int t;
+    cin >> t;
+ 
+    while(t--)
+    {
+        ans();
+    }
+ 
+    return 0;
+}
