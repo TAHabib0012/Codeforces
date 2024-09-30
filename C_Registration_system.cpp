432f8924f62e7642010a03a53da809d5cc9dc7d6
Tahmid Ahmed Habib <tahmidahmed0012@gmail.com>
2024-09-30 17:15:23 +0600

    Solved

---
+++ C_Registration_system.cpp
@@ -0,0 +1,61 @@
+/*............Bismillahir Rahmanir Raheem...............*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define Tahmid ios::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr);
+#define LL long long int
+#define ULL unsigned long long 
+#define endl '\n'
+
+#define mem(v, x) memset(v, x, sizeof(v))
+#define ceiling(a, b) ((a / b) + (a % b != 0))
+#define gcd(a, b) __gcd(a, b)
+#define lcm(a, b) ((a * b) / gcd(a, b))
+
+#define yes cout << "YES" << endl;
+#define no cout << "NO" << endl;
+#define HA " "
+
+const LL NN = 1e9 + 6 + 9;
+const LL mod = 998244353;
+
+void ans()
+{
+    int n, c = 0;
+    cin >> n;
+    map<string, int> mp;
+
+    for (int i = 0; i < n; i++)
+    {
+        string s;
+        cin >> s;
+        
+        if (mp.find(s) == mp.end())
+        {
+            cout << "OK" << endl;
+            mp[s] = 1;
+        }
+        else
+        {
+            mp[s]++;
+            cout << s << mp[s] - 1 << endl;
+
+        }
+    }
+}
+
+int main() 
+{
+    Tahmid;
+
+    int t = 1;
+    //cin >> t;
+ 
+    while(t--)
+    {
+        ans();
+    }
+ 
+    return 0;
+}
