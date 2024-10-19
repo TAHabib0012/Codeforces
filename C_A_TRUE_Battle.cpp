d7de39257fe6d3d2ccfd427cb63ff4690b6413d2
Tahmid Ahmed Habib <tahmidahmed0012@gmail.com>
2024-10-20 00:55:29 +0600

    Solved

---
+++ C_A_TRUE_Battle.cpp
@@ -0,0 +1,69 @@
+/*............Bismillahir Rahmanir Raheem...............*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define Tahmid ios::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr);
+#define LL long long int
+#define ULL unsigned long long 
+#define endl '\n'
+
+#define mem(v, x) memset(v, x, sizeof(v))
+#define ceiling(a, b) ((a / b) + (a % b != 0))
+#define gcd(a, b) __gcd(a, b)
+#define lcm(a, b) ((a * b) / gcd(a, b))
+
+#define yes cout << "YES" << endl;
+#define no cout << "NO" << endl;
+#define HA " "
+
+const LL NN = 1e9 + 6 + 9;
+const LL mod = 998244353;
+
+void ans()
+{
+    int n;
+    cin >> n;
+    string s;
+    cin >> s;
+    
+    bool hae = false;
+    for (int i = 0; i < n - 1; i++)
+    {
+        if (s[i] == '1' && s[i + 1] == '1')
+        {
+            hae = true;
+            break;
+        }
+    }
+    
+    if (s[0] == '1' || s[n - 1] == '1')
+    {
+        hae = true;
+    }
+
+    if (hae)
+    {
+        yes;
+    }
+    else
+    {
+        no;
+    }
+}
+
+int main() 
+{
+    Tahmid;
+
+    int t;
+    cin >> t;
+ 
+    while(t--)
+    {
+        ans();
+    }
+ 
+    return 0;
+}
+
