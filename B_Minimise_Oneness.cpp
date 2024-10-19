ba1ab2ec5c4b61b198bbec7a69ada58a853f71d1
Tahmid Ahmed Habib <tahmidahmed0012@gmail.com>
2024-10-19 20:54:16 +0600

    Solved

---
+++ B_Minimise_Oneness.cpp
@@ -0,0 +1,59 @@
+/*............Bismillahir Rahmanir Raheem...............*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define Tahmid ios::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr);
+#define LL long long int
+#define ULL unsigned long long 
+#define endl '\n'
+
+#define mem(v, x) memset(v, x, sizeof(v))
+#define ceiling(a, b) ((a / b) + (a % b != 0))
+#define gcd(a, b) __gcd(a, b)
+#define lcm(a, b) ((a * b) / gcd(a, b))
+
+#define yes cout << "YES" << endl;
+#define no cout << "NO" << endl;
+#define HA " "
+
+const LL NN = 1e9 + 6 + 9;
+const LL mod = 998244353;
+
+void ans()
+{
+    int n;
+    cin >> n;
+    string s = "";
+
+    for (int i = 0; i < n; i++)
+    {
+        if (i < n - 1)
+        {
+            s = s +'0';
+        }
+        else
+        {
+            s = s + '1';
+        }
+    }
+    cout << s << endl;
+}
+
+int main() 
+{
+    Tahmid;
+
+    int t;
+    cin >> t;
+ 
+    while(t--)
+    {
+        ans();
+    }
+ 
+    return 0;
+}
+
+
+
