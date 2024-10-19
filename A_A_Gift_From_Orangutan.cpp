ef296ef44bbe14812ca053886c2b09e703e940ad
Tahmid Ahmed Habib <tahmidahmed0012@gmail.com>
2024-10-19 20:44:46 +0600

    Solved

---
+++ A_A_Gift_From_Orangutan.cpp
@@ -0,0 +1,55 @@
+/*............Bismillahir Rahmanir Raheem...............*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define Tahmid ios::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr);
+#define LL long long int
+#define ULL unsigned long long 
+#define endl '\n'
+
+#define mem(v, x) memset(v, x, sizeof(v))
+#define ceiling(a, b) ((a / b) + (a % b != 0))
+#define gcd(a, b) __gcd(a, b)
+#define lcm(a, b) ((a * b) / gcd(a, b))
+
+#define yes cout << "YES" << endl;
+#define no cout << "NO" << endl;
+#define HA " "
+
+const LL NN = 1e9 + 6 + 9;
+const LL mod = 998244353;
+
+void ans()
+{
+    int n;
+    cin >> n;
+    vector<int> a(n), b(n), c(n);
+
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a[i];
+    }
+    int x = *max_element(a.begin(), a.end());
+    int y = *min_element(a.begin(), a.end());
+
+    x = x * (n - 1);
+    y = y * (n - 1);
+
+    cout << x - y << endl;
+}
+
+int main() 
+{
+    Tahmid;
+
+    int t;
+    cin >> t;
+ 
+    while(t--)
+    {
+        ans();
+    }
+ 
+    return 0;
+}
