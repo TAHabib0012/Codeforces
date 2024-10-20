c599343398ecf622d12c10a18ae12e875825ec77
Tahmid Ahmed Habib <tahmidahmed0012@gmail.com>
2024-10-20 16:40:42 +0600

    Solved

---
+++ A_Profitable_Interest_Rate.cpp
@@ -0,0 +1,63 @@
+/*............Bismillahir Rahmanir Raheem...............*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define Tahmid ios::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr);
+#define LL long long int
+#define ULL unsigned long long 
+#define endl '\n'
+
+#define mem(v, x) memset(v, x, sizeof(v))
+#define ceiling(a, b) ((a / b) + (a % b != 0))
+#define gcd(a, b) __gcd(a, b)
+#define lcm(a, b) ((a * b) / gcd(a, b))
+
+#define yes cout << "YES" << endl;
+#define no cout << "NO" << endl;
+#define HA " "
+
+const LL NN = 1e9 + 6 + 9;
+const LL mod = 998244353;
+
+void ans()
+{
+    LL n, m, s = 0;
+    cin >> n >> m;
+
+    if (n >= m)
+    {
+        s = n;
+    }
+    else
+    {
+        LL f = m -n;
+
+        if (n - f > 0)
+        {
+            s = n - f;
+        }
+        else
+        {
+            s = 0;
+        }
+        
+    }
+    
+    cout << s << endl;
+}
+
+int main() 
+{
+    Tahmid;
+
+    int t;
+    cin >> t;
+ 
+    while(t--)
+    {
+        ans();
+    }
+ 
+    return 0;
+}
