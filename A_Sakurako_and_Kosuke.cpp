4ae6181ec945980022aace75e71f99a9617d51e1
Tahmid Ahmed Habib <tahmidahmed0012@gmail.com>
2024-10-26 20:39:16 +0600

    Solved

---
+++ A_Sakurako_and_Kosuke.cpp
@@ -0,0 +1,72 @@
+/*............Bismillahir Rahmanir Raheem...............*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define Tahmid ios::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr);
+#define LL long long int
+#define ULL unsigned long long 
+#define endl '\n'
+
+#define mem(v, x) memset(v, x, sizeof(v))
+#define ceiling(a, b) ((a / b) + (a % b != 0))
+#define gcd(a, b) __gcd(a, b)
+#define lcm(a, b) ((a * b) / gcd(a, b))
+
+#define yes cout << "YES" << endl;
+#define no cout << "NO" << endl;
+#define HA " "
+
+const LL NN = 1e9 + 6 + 9;
+const LL mod = 998244353;
+
+void ans()
+{
+    int n, c = 0, f = 1;
+    cin >> n;
+        
+        
+    while (true) 
+    {
+        int x = 2 * f - 1;
+            
+        if (f % 2 == 1) 
+        {
+            c = c - x;
+            if (abs(c) > n) 
+            {
+                cout << "Sakurako" << endl;
+                return;
+            }
+        } 
+        else 
+        {
+            c = c + x;
+            if (abs(c) > n) 
+            {
+                cout << "Kosuke" << endl;
+                return;
+            }
+
+        }
+            
+            
+        ++f;
+    }
+}
+
+
+int main() 
+{
+    Tahmid;
+
+    int t;
+    cin >> t;
+ 
+    while(t--)
+    {
+        ans();
+    }
+ 
+    return 0;
+}
