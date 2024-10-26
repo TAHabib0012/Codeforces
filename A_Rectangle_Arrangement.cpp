6e822d56feb32d7ef6c0e83f33b6adbdc416e0ef
Tahmid Ahmed Habib <tahmidahmed0012@gmail.com>
2024-10-27 00:29:29 +0600

    Solved

---
+++ A_Rectangle_Arrangement.cpp
@@ -0,0 +1,55 @@
+/*............Bismillahir Rahmanir Raheem...............*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define Tahmid ios::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr);
+#define LL long long int
+#define ULL unsigned long long 
+#define endl '\n'
+
+#define mem(v, x) memset(v, x, sizeof(v))
+#define ceiling(a, b) ((a / b) + (a % b != 0))
+#define gcd(a, b) __gcd(a, b)
+#define lcm(a, b) ((a * b) / gcd(a, b))
+
+#define yes cout << "YES" << endl;
+#define no cout << "NO" << endl;
+#define HA " "
+
+const LL NN = 1e9 + 6 + 9;
+const LL mod = 998244353;
+
+void ans()
+{
+    int n, c = 0, f = 0;
+    cin >> n;
+
+    while (n > 0)
+    {
+        int x, y;
+        cin >> x >> y;
+
+        c = max(c,x);
+        f = max(f, y);
+        n--;
+    }
+    
+    cout << 2 * (c + f) << endl;
+
+}
+
+int main() 
+{
+    Tahmid;
+
+    int t;
+    cin >> t;
+ 
+    while(t--)
+    {
+        ans();
+    }
+ 
+    return 0;
+}
