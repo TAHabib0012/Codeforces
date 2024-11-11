6aa48d1862697cd5c48a82c7b7c306be84c642db
Tahmid Ahmed Habib <tahmidahmed0012@gmail.com>
2024-11-11 23:41:11 +0600

    Solved

---
+++ A_A_B_Again.cpp
@@ -0,0 +1,49 @@
+/*............Bismillahir Rahmanir Raheem...............*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define Tahmid ios::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr);
+#define LL long long int
+#define ULL unsigned long long 
+#define endl '\n'
+
+#define mem(v, x) memset(v, x, sizeof(v))
+#define ceiling(a, b) ((a / b) + (a % b != 0))
+#define gcd(a, b) __gcd(a, b)
+#define lcm(a, b) ((a * b) / gcd(a, b))
+
+#define yes cout << "YES" << endl;
+#define no cout << "NO" << endl;
+#define HA " "
+
+const LL NN = 1e9 + 6 + 9;
+const LL mod = 998244353;
+
+void ans()
+{
+    LL n, c = 0;
+    cin >> n;
+
+    while (n != 0)
+    {
+        c =  c + (n % 10);
+        n = n / 10;
+    }
+    cout << c << endl;
+}
+
+int main() 
+{
+    Tahmid;
+
+    int t;
+    cin >> t;
+ 
+    while(t--)
+    {
+        ans();
+    }
+ 
+    return 0;
+}
